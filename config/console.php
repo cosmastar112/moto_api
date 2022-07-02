6cb1d6cff4eac4ac81f14b05ccf2901cb74e0422
cosmastar112 <cosmastar112@gmail.com>
2022-07-02 18:25:19 +0400

    таймзона сервера - МСК

--- config/console.php
+++ config/console.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic-console',
+    'timeZone' => 'Europe/Moscow',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'app\commands',
