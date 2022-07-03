7e42540f0e584946b46469269d61aad1156c0a66
cosmastar112 <cosmastar112@gmail.com>
2022-07-03 12:50:37 +0400

    конфиг и компонент тестовой БД

--- config/console.php
+++ config/console.php
@@ -2,6 +2,7 @@
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
+$test_db = require __DIR__ . '/test_db.php';
 
 $config = [
     'id' => 'basic-console',
@@ -27,6 +28,7 @@ $config = [
             ],
         ],
         'db' => $db,
+        'test_db' => $test_db,
     ],
     'params' => $params,
     /*
