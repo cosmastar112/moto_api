bfe83a5665ab600beee1cb207c3855c01ae3a53d
cosmastar112 <cosmastar112@gmail.com>
2022-07-18 20:34:32 +0400

    fix: конфиг тестовой БД переписывал конфиг (DSN) рабочей БД

--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,14 @@
 <?php
-$db = require __DIR__ . '/db.php';
-// test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=db;dbname=moto_test';
 
-return $db;
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=moto_test',
+    'username' => 'moto',
+    'password' => 'password',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
